9b62e27429770700f97f963a036b3ad6560b993a
Bret McKee <bret.mckee@hpe.com>
2021-06-30 08:48:17 -0700

    Enhance v1 volume-attach.go (#11)
    * Enhance v1 volume-attach.go
    
    This commit adds the structure necessary to create volume attachements.
    There is currently no Quake volume attach create REST support, but in
    order to generate it this file is required, so this change is being made
    by itself. Once the Quake side is implemented, subsequent commits to
    this repository will add client support.
    ___
    bretmckee-branch: enhance-volume-attach
    
    * address llabordehpe PR comments
    
    * Fix lint found typo
    
    * address llabordehpe PR comments

--- v1/pkg/model/volume-attach.go
+++ v1/pkg/model/volume-attach.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2020 Hewlett Packard Enterprise Development LP.
+// Copyright 2016-2021 Hewlett Packard Enterprise Development LP.
 
 package model
 
@@ -18,6 +18,54 @@ type VolumeAttachment struct {
 	// ArrayNetworkInterfaceID string // ArrayNetworkInterfaceID this must be initialized at creation time; it must correspond to an arry netoworkID that is reported in // StorageNetwork.ArrayNetworkInterfaceID
 	// ArrayVolumeAttachmentID string // Array parameters that may be passed to the driver with casts. These fields are populated by the storagemon bot and should be // unintialized at point of creation.
 	// ArrayHostID             string // ArrayHostID is a reference to the array-side host entry.
+}
+
+// ISCSIParameters contains the parameters required to attach a volume using
+// ISCSI.
+type ISCSIParameters struct {
+	// HostIPAddress is the IPV4 IP Address of the iSCSI initiator (host) in dot
+	// notation or FQN.
+	HostIPAddress string
+	// IQN is the full initiator name used for identification during iSCSI login.
+	InitiatorName string
+	// CHAPSecret is the Challenge Authentication Protocol secret to be shared
+	// between array and initiator. If empty, no CHAP login is enabled; if set it
+	// must be a string between 12 and 16 characters.
+	CHAPSecret string
+	// CHAPUserName is the CHAP username to use for CHAP authentication.
+	CHAPUserName string
+}
+
+// ProtocolEnum defines the supported attachment protocols.
+type ProtocolEnum string
+
+// Protocol Enum Value
+const (
+	ProtocolKindUnknown = "unknown"
+	ProtocolKindISCSI   = "iscsi"
+)
+
+// ProtocolParameters contains the information to attach a volume using the
+// specified protocol.
+type ProtocolParameters struct {
+	// Protocol specifies the protocol to be used to attach the volume. See the
+	// protocol specific parameters below for legal values.
+	Protocol ProtocolEnum
+	// ISCSI contains the information required to attach the volume using ISCSI.
+	// It must be specified if Protocol is ProtocolKindISCSI.
+	ISCSI ISCSIParameters
+}
+
+// NewVolumeAttachment is used to create an attachment between a non-Quake
+// controlled machine (i.e. a machine that does not have a HostID) and a volume.
+type NewVolumeAttachment struct {
+	// ResourceBase contains common elements of Quake Objects. The caller must set
+	// Name and should set Description.
+	ResourceBase
+	// VolumeID identifies the volume attach.
+	VolumeID string
+	// Protocol specifies the protocol specific information for this attachment.
+	Protocol ProtocolParameters
 }
 
 // VolumeAttachmentHostUuid represents the host that a volume is being attached to or
