02d32bdd78fffb45ed5b5c9e41ec0cd1c1adacfb
chaitra-mylarappachar <65302544+chaitra-mylarappachar@users.noreply.github.com>
2022-10-03 21:57:40 +0000

    Merge latest master into feature (#40)
    * Misc issues (#36)
    
    * OTEL - Metal Telemetry Service - Add "Telemetry" network purpose (#38)
    
    Rally: US41549
    
    Co-authored-by: Patricia DiBenedetto <patricia.dibenedetto@hpe.com>

--- v1/pkg/client/interface_allocation.go
+++ v1/pkg/client/interface_allocation.go
@@ -15,10 +15,11 @@ type AllocationAPI interface {
 	   GetBySite Get servers allocation
 	   Returns allocation information for each server instance type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
 	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param siteID site ID
+	    * @param optional nil or *AllocationApiGetBySiteOpts - Optional Parameters:
+	    * @param "SiteID" (optional.String) -  site ID
 	   @return Allocation
 	*/
-	GetBySite(ctx _context.Context, siteID string) (Allocation, *_nethttp.Response, error)
+	GetBySite(ctx _context.Context, localVarOptionals *AllocationApiGetBySiteOpts) (Allocation, *_nethttp.Response, error)
 	/*
 	   StorageGetBySite Get storage allocation
 	   Returns allocation information for each volume type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
