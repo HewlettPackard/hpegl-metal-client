9d4569d1e9b7f2f5a1c8317295b3194d8941b8a6
Anuradha Bose <anuradha.bose@hpe.com>
2023-01-12 14:41:09 -0600

    updated the doc for allocation by project

--- v1/pkg/client/interface_allocation.go
+++ v1/pkg/client/interface_allocation.go
@@ -13,7 +13,7 @@ import (
 type AllocationAPI interface {
 	/*
 	   GetBySite Get servers allocation
-	   Returns an array of allocation information for each server instance type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
+	   Returns an array of allocation information for each server instance type used by each PCE service. If siteID is present, the information returned is specific to that site ID. If the allocation data is needed for a specific project, then projectID in the request header must be present. In other case, the allocation information for all sites is returned.
 	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	    * @param optional nil or *AllocationApiGetBySiteOpts - Optional Parameters:
 	    * @param "SiteID" (optional.String) -  site ID
@@ -22,7 +22,7 @@ type AllocationAPI interface {
 	GetBySite(ctx _context.Context, localVarOptionals *AllocationApiGetBySiteOpts) ([]Allocation, *_nethttp.Response, error)
 	/*
 	   StorageGetBySite Get storage allocation
-	   Returns an array of allocation information for each volume type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
+	   Returns an array of allocation information for each volume type used by each PCE service. If siteID is present, the information returned is specific to that site ID. If the allocation data is needed for a specific project, then projectID in the request header must be present. In other case, the allocation information for all sites is returned.If the allocation data is needed for a specific project, then projectID in the request header must be present. In other case, the allocation information for all sites is returned.
 	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	    * @param optional nil or *AllocationApiStorageGetBySiteOpts - Optional Parameters:
 	    * @param "SiteID" (optional.String) -  site ID
