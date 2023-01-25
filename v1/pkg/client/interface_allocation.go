5d047d9837cde8ebf9418b8c50cf536f2b9178d7
Anuradha Bose <anuradha.bose@hpe.com>
2023-01-25 10:40:25 -0600

    add doc for allocation api by project

--- v1/pkg/client/interface_allocation.go
+++ v1/pkg/client/interface_allocation.go
@@ -13,7 +13,7 @@ import (
 type AllocationAPI interface {
 	/*
 	   GetBySite Get servers allocation
-	   Returns an array of allocation information for each server instance type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
+	   Returns an array of allocation information for each server instance type. Each instance type&#39;s allocation information is further grouped by service type. When using a Metal token, the value in the &#39;Membership&#39; header determines the scope of the response, i.e., Project or Hoster. However, the default scope is Tenant/Hoster when using the GL IAM token. If the allocation data is needed for a specific project, then the &#39;Project&#39; header must be present in the request. If the &#39;siteID&#39; query parameter is present, the information returned is specific to that site ID.
 	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	    * @param optional nil or *AllocationApiGetBySiteOpts - Optional Parameters:
 	    * @param "SiteID" (optional.String) -  site ID
@@ -22,7 +22,7 @@ type AllocationAPI interface {
 	GetBySite(ctx _context.Context, localVarOptionals *AllocationApiGetBySiteOpts) ([]Allocation, *_nethttp.Response, error)
 	/*
 	   StorageGetBySite Get storage allocation
-	   Returns an array of allocation information for each volume type used by each PCE service. If siteID is present, the information returned is specific to that site ID, otherwise the allocation information for all sites is returned.
+	   Returns an array of allocation information for each volume instance type. Each instance type&#39;s allocation information is further grouped by service type. When using a Metal token, the value in the &#39;Membership&#39; header determines the scope of the response, i.e., Project or Hoster. However, the default scope is Tenant/Hoster when using the GL IAM token. If the allocation data is needed for a specific project, then the &#39;Project&#39; header must be present in the request. If the &#39;siteID&#39; query parameter is present, the information returned is specific to that site ID.
 	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 	    * @param optional nil or *AllocationApiStorageGetBySiteOpts - Optional Parameters:
 	    * @param "SiteID" (optional.String) -  site ID
