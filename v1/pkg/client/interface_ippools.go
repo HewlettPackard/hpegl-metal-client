97bf07da82ad2eb930c2dadea9c2087cbbfa67de
Anuradha Bose <anuradha.bose@hpe.com>
2023-02-08 13:54:25 -0600

    change  storage allocated capacity float

--- v1/pkg/client/interface_ippools.go
+++ v1/pkg/client/interface_ippools.go
@@ -14,42 +14,47 @@ type IPPoolsAPI interface {
 	/*
 	   AllocateIPs Allocate IPs from the pool
 	   Allocate IPs from the pool
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param ippoolId ID of IP pool to allocate IPs
-	    * @param iPAllocation IPs being requested starting from an optional base IP and their usage
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ippoolId ID of IP pool to allocate IPs
+	     - @param iPAllocation IPs being requested starting from an optional base IP and their usage
+
 	   @return IpPool
 	*/
 	AllocateIPs(ctx _context.Context, ippoolId string, iPAllocation []IpAllocation) (IpPool, *_nethttp.Response, error)
 	/*
 	   GetByID Retrieve IP pool by ID
 	   Returns a single ip pool with matching ID
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param ippoolId ID of IP pool to return
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ippoolId ID of IP pool to return
+
 	   @return IpPool
 	*/
 	GetByID(ctx _context.Context, ippoolId string) (IpPool, *_nethttp.Response, error)
 	/*
 	   List List all ip pools in project
 	   Returns an array of all ip pool objects defined within the project.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+
 	   @return []IpPool
 	*/
 	List(ctx _context.Context) ([]IpPool, *_nethttp.Response, error)
 	/*
 	   ReturnIPs Return IPs to the pool
 	   Return IPs to the pool
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param ippoolId ID of IP pool to return IPs
-	    * @param requestBody IP returned to the pool
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ippoolId ID of IP pool to return IPs
+	     - @param requestBody IP returned to the pool
+
 	   @return IpPool
 	*/
 	ReturnIPs(ctx _context.Context, ippoolId string, requestBody []string) (IpPool, *_nethttp.Response, error)
 	/*
 	   Update Update IP pool by ID
 	   Update a single ip pool with matching ID. &#39;DefaultRoute&#39; can only be updated if ip pool is not currently in-use.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param ippoolId ID of IP pool to update
-	    * @param ipPool Update IPPool
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ippoolId ID of IP pool to update
+	     - @param ipPool Update IPPool
+
 	   @return IpPool
 	*/
 	Update(ctx _context.Context, ippoolId string, ipPool IpPool) (IpPool, *_nethttp.Response, error)
--- v1/pkg/client/interface_volumes.go
+++ v1/pkg/client/interface_volumes.go
@@ -14,55 +14,60 @@ type VolumesAPI interface {
 	/*
 	   Add Add a new volume
 	   Adds a new volume to the project.  Volumes may be created separately and then referenced in the create Host call; or volumes may be created directly within the create Host call.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param newVolume Volume that is to be added to the project
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param newVolume Volume that is to be added to the project
+
 	   @return Volume
 	*/
 	Add(ctx _context.Context, newVolume NewVolume) (Volume, *_nethttp.Response, error)
 	/*
 	   Attach Attach existing volume to Host
 	   Attaches the indicated volume to a host identified in the requestBody.   This attachment will create a VolumeAttachment object that contains  details about the connection of the volume and will update the Host  with iSCSI configuration information.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param volumeId ID of volume to attach
-	    * @param volumeAttachHostUuid Unique ID of the Host to which the volume will be attached
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param volumeId ID of volume to attach
+	     - @param volumeAttachHostUuid Unique ID of the Host to which the volume will be attached
+
 	   @return VolumeAttachment
 	*/
 	Attach(ctx _context.Context, volumeId string, volumeAttachHostUuid VolumeAttachHostUuid) (VolumeAttachment, *_nethttp.Response, error)
 	/*
 	   Delete Delete a volume
 	   Deletes the volume with the matching ID
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param volumeId ID of volume to delete
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param volumeId ID of volume to delete
 	*/
 	Delete(ctx _context.Context, volumeId string) (*_nethttp.Response, error)
 	/*
 	   Detach Detach existing volume from Host
 	   Detaches the indicated volume from the host identified in the requestBody.   This detachment will delete the VolumeAttachment object that contains  details about the connection of the volume and will update the Host  to remove selected iSCSI configuration information. Note that the HostID is required in the body of the request to ensure that the operation is well understood and that a volume is not accidently being removed from the wrong host.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param volumeId ID of volume to attach
-	    * @param volumeAttachHostUuid Unique ID of the Host from which a volume will be detached
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param volumeId ID of volume to attach
+	     - @param volumeAttachHostUuid Unique ID of the Host from which a volume will be detached
 	*/
 	Detach(ctx _context.Context, volumeId string, volumeAttachHostUuid VolumeAttachHostUuid) (*_nethttp.Response, error)
 	/*
 	   GetByID Retrieve volume by ID
 	   Returns a single volume with matching ID
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param volumeId ID of volume to return
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param volumeId ID of volume to return
+
 	   @return Volume
 	*/
 	GetByID(ctx _context.Context, volumeId string) (Volume, *_nethttp.Response, error)
 	/*
 	   List List all volumes in project
 	   Returns an array of all volumes defined within the project.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+
 	   @return []Volume
 	*/
 	List(ctx _context.Context) ([]Volume, *_nethttp.Response, error)
 	/*
 	   Update Update an existing volume
 	   Updates volume with matching ID. Update is permitted only when volume is in &#39;Allocated&#39; or &#39;Visible&#39; state. Only the Volume &#39;Capacity&#39; can be updated with a value greater than the existing one to expand the volume.
-	    * @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-	    * @param volume Volume object with its ID and Capacity in GiB indicating the expanded size to be specified.
+	     - @param ctx _context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	     - @param volume Volume object with its ID and Capacity in GiB indicating the expanded size to be specified.
+
 	   @return Volume
 	*/
 	Update(ctx _context.Context, volume Volume) (Volume, *_nethttp.Response, error)
