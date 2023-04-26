8994357e8ce86a4dd39bfbf1037580c963ac41a9
Denis Bozzato <denis.bozzato@hpe.com>
2023-04-26 16:29:01 +0100

    US49481-Implemented review comments

--- v1/pkg/client/model_location_info.go
+++ v1/pkg/client/model_location_info.go
@@ -19,8 +19,8 @@ type LocationInfo struct {
 	Region string `json:"Region"`
 	// Data center descriptor
 	DataCenter string `json:"DataCenter"`
-	// Indicates whether or not the fabric dedicated for private networks requires VLAN ID.
-	PrivateNetworkVIDRequired bool `json:"PrivateNetworkVIDRequired"`
-	// Indicates whether or not the fabric dedicated for private networks requires VNI ID.
-	PrivateNetworkVNIRequired bool `json:"PrivateNetworkVNIRequired"`
+	// Indicates whether or not the fabric dedicated for private networks supports VLAN ID.
+	PrivateNetworkVIDSupport bool `json:"PrivateNetworkVIDSupport"`
+	// Indicates whether or not the fabric dedicated for private networks supports VNI ID.
+	PrivateNetworkVNISupport bool `json:"PrivateNetworkVNISupport"`
 }
