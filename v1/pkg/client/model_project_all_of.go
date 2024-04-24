fd45cdf9981c22668bc8c1a339bfe98d1fa0d66c
“Patricia <patricia.dibenedetto@hpe.com>
2024-04-24 13:08:18 -0500

    initial code

--- v1/pkg/client/model_project_all_of.go
+++ v1/pkg/client/model_project_all_of.go
@@ -19,4 +19,6 @@ type ProjectAllOf struct {
 	PermittedSites []string `json:"PermittedSites"`
 	// Array listing the permitted OS service images.
 	PermittedOSImages []string `json:"PermittedOSImages"`
+	// Indicates whether volume replication is enabled for this Project
+	ReplicationEnabled bool `json:"ReplicationEnabled,omitempty"`
 }
